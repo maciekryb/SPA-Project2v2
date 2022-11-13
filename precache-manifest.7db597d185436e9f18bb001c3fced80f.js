9bbedb2b7b86b61bb8b007671723fc8a4d7d7fc5
Maciej Rybak <maciek.rybak95@gmail.com>
2022-11-13 09:38:44 +0100

    Updates

---
+++ precache-manifest.7db597d185436e9f18bb001c3fced80f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f17a91f5bfe56870b836",
+    "url": "/SPA-Project2/static/css/main.991ff365.chunk.css"
+  },
+  {
+    "revision": "f17a91f5bfe56870b836",
+    "url": "/SPA-Project2/static/js/main.f17a91f5.chunk.js"
+  },
+  {
+    "revision": "c8732e009cbffad1070f",
+    "url": "/SPA-Project2/static/js/runtime~main.c8732e00.js"
+  },
+  {
+    "revision": "17885c5e28dae2c28ae2",
+    "url": "/SPA-Project2/static/js/2.17885c5e.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/SPA-Project2/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/SPA-Project2/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/SPA-Project2/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "f3b10ef476f5dbee39d3eeed2e3e8b24",
+    "url": "/SPA-Project2/index.html"
+  }
+];
