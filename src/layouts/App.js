3063db936c7aa5a726fd10281d12caf5f5f464c0
Maciej Rybak <maciek.rybak95@gmail.com>
2022-11-13 10:03:49 +0100

    Add gh-pages

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -9,12 +9,12 @@ import Footer from './Footer';
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className='app'>
+      <Router basename={process.env.PUBLIC_URL}>
+        <div className="app">
           <header>{<Header />}</header>
           <main>
             <aside>{<Navigation />}</aside>
-            <section className='page'>{<Page />}</section>
+            <section className="page">{<Page />}</section>
           </main>
           <footer>{<Footer />}</footer>
         </div>
